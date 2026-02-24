81d0bec71367bd25b81a07b060002dc6bd417f20
Ethan Kemp <ethanrandelkemp@gmail.com>
2026-02-24 16:55:17 +1300

    Added 'reset' option that resets your money

--- src/usage.c
+++ src/usage.c
@@ -5,12 +5,14 @@ void usage(char *arg0)
 {
 	printf("Usage: %s [options] mode...\n", arg0);
 	printf("Options:\n");
-	printf("  -h,--help\tPrint this message\n");
+	printf("  -h,--help   \tPrint this message\n");
 	printf("  -v,--version\tPrint the version of the casino.\n");
 	
 	printf("\nModes:\n");
+	printf("  reset   \tFully reset your money count.\n\n");
+
 	printf("  roulette\tPlay roulette.\n");
-	printf("  slots   \tPlay slot machines.\n"); /*
-	printf("  bj      \tPlay blackjack.\n");
-	printf("  poker   \tPlay poker.\n"); */
+	printf("  slots   \tPlay slot machines.\n");
+	// printf("  bj      \tPlay blackjack.\n");
+	// printf("  poker   \tPlay poker.\n");
 }
